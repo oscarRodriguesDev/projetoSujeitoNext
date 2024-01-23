37132370b9d4d040b02d61128951772ccbfb983d
Oscar <oskharm12@gmail.com>
2024-01-22 22:06:23 -0300

    footer criado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import styles from "../styles/home.module.scss";
+import Image from "next/image";
 import Head from "next/head";
+import TecsImage from "../../public/images/techs.svg";
 export default function Home() {
   return (
     <>
@@ -30,21 +32,46 @@ export default function Home() {
         <div className={styles.sectionContent}>
           <section>
             <h2>Aprenda a criar aplicativos com Android e IOS</h2>
-            <span>Você vai descobrir o jeito mais moderno apps nativos para IOS e Android, construindo aplicativos do zero até aplicativos</span>
+            <span>
+              Você vai descobrir o jeito mais moderno apps nativos para IOS e
+              Android, construindo aplicativos do zero até aplicativos
+            </span>
           </section>
-          <img src="/images/financasApp.png" alt="conteudo de desenvolvimentos de apps" />
+          <img
+            src="/images/financasApp.png"
+            alt="conteudo de desenvolvimentos de apps"
+          />
         </div>
 
         <hr className={styles.divisor} />
 
         <div className={styles.sectionContent}>
-          <img src="/images/webDev.png" alt="conteudo de desenvolvimentos de aplicações web" />
+          <img
+            src="/images/webDev.png"
+            alt="conteudo de desenvolvimentos de aplicações web"
+          />
           <section>
             <h2>Aprenda a criar Sistemas Web</h2>
-            <span>Criar sistemas e sites usando as tecnologias mais modernas do mercado</span>
+            <span>
+              Criar sistemas e sites usando as tecnologias mais modernas do
+              mercado
+            </span>
           </section>
         </div>
 
+        <div className={styles.nextLevelContent}>
+          <Image src={TecsImage} alt="Tecnologias" />
+          <h2>
+            Mais de <span className={styles.alunos}>15 mil </span> ja levaram suas carreiras ao proximo
+            nível
+          </h2>
+          <span>
+            E você vai perder a chance de evoluir de uma vez por todas?
+          </span>
+          <a href='#'>
+            <button>ACESSAR TURMA!</button>
+          </a>
+        </div>
       </main>
     </>
   );
